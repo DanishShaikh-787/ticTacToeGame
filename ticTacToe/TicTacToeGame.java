95a0f8aa54215abd5f9dddd7357028a7f3d688a7
Danish Shaikh <dishaikh1994@gmail.com>
2020-11-05 10:52:00 +0530

    Added : Create Empty Board

--- ticTacToe/TicTacToeGame.java
+++ ticTacToe/TicTacToeGame.java
@@ -3,8 +3,13 @@ package ticTacToe;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[] board = createBoard();
 	}
-
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for (int i=0; i<board.length; i++) {
+			board [i] =' ' ;
+		}
+		return board;
+	}
 }
